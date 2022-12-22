bf2b5a98b65c81938d1602f934e2ea861e3d29b1
ohoud-almehmadi <ohoudmehmadi@outlook.com>
2022-12-21 17:05:42 -0800

    second1

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://myudagramapi-env.eba-b36hcwkn.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://myudagramapi-env.eba-b36hcwkn.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
