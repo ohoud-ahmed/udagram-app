8f50e92823b32461d09c00790a550c51e30c2085
ohoud-almehmadi <ohoudmehmadi@outlook.com>
2022-12-22 09:06:59 -0800

    first commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,5 +1,5 @@
 # This file is used for convenience of local development.
-# DO NOT STORE YOUR CREDENTIALS INTO GIT
+# DO NOT STOREYOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
 export POSTGRES_HOST=database-1.cqzmuhnfv5up.us-east-1.rds.amazonaws.com
